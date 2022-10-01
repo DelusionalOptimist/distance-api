bc5e6ac6f31904fa8c70b47a819ec1f2e4ad8119
Rudraksh Pareek <rudrakshpareek3601@gmail.com>
2022-10-01 17:05:24 +0530

    add autocomplete
    Signed-off-by: Rudraksh Pareek <rudrakshpareek3601@gmail.com>

--- static/script.js
+++ static/script.js
@@ -1,8 +1,9 @@
-let form = document.getElementById("location_data");
-let distance = document.getElementById("distance_box")
+const form = document.getElementById("location-data");
+const distance = document.getElementById("result-box")
+const placesInput = document.getElementsByClassName("pac-input");
 
-form.addEventListener('submit', handleSubmit);
-function handleSubmit(event) {
+form.addEventListener("submit", getDistance);
+function getDistance(event) {
 	event.preventDefault();
 	const data = new FormData(event.target);
 	const formJSON = Object.fromEntries(data.entries());
@@ -38,13 +39,138 @@ function initMap() {
 		}
 	);
 
+	// for displaying route
 	directionsRenderer.setMap(map);
 	const onChangeHandler = function () {
 		calculateAndDisplayRoute(directionsService, directionsRenderer);
 	};
 
+	const options = {
+		// TODO:look into fields
+		fields: ["formatted_address", "geometry", "name"],
+		strictBounds: false,
+		types: ["establishment"],
+	};
+
+	let placesAutocomplete = {};
+	for (let i = 0; i < placesInput.length; i++) {
+		placesAutocomplete[i] = new google.maps.places.Autocomplete(placesInput[i], options);
+
+		placesAutocomplete[i].addListener("place_changed", () => {
+			//infowindow.close();
+			//marker.setVisible(false);
+
+			const place = placesAutocomplete[i].getPlace();
+
+			if (!place.geometry || !place.geometry.location) {
+				// User entered the name of a Place that was not suggested and
+				// pressed the Enter key, or the Place Details request failed.
+				window.alert("No details available for input: '" + place.name + "'");
+				return;
+			}
+
+			// If the place has a geometry, then present it on a map.
+			//if (place.geometry.viewport) {
+			//	map.fitBounds(place.geometry.viewport);
+			//} else {
+			//	map.setCenter(place.geometry.location);
+			//	map.setZoom(17);
+			//}
+
+			//marker.setPosition(place.geometry.location);
+			//marker.setVisible(true);
+
+			//infowindowContent.children["place-name"].textContent = place.name;
+			//infowindowContent.children["place-address"].textContent =
+			//	place.formatted_address;
+			//infowindow.open(map, marker);
+		});
+
+	}
+
 	form.addEventListener("submit", onChangeHandler);
-	form.addEventListener("submit", onChangeHandler);
+
+	//const infowindow = new google.maps.InfoWindow();
+	//const infowindowContent = document.getElementById(
+	//	"infowindow-content"
+	//);
+	//infowindow.setContent(infowindowContent);
+
+	//const marker = new google.maps.Marker({
+	//	map,
+	//	anchorPoint: new google.maps.Point(0, -29),
+	//});
+
+	//const origin = document.getElementById("pac-origin");
+	//const destination = document.getElementById("pac-destination");
+	//const originAutocomplete = new google.maps.places.Autocomplete(origin, options);
+
+	//const destinationAutocomplete = new google.maps.places.Autocomplete(destination, options);
+
+
+	//var originPlace,destinationPlace;
+
+	//originAutocomplete.addListener("place_changed", () => {
+	//	infowindow.close();
+	//	marker.setVisible(false);
+
+	//	originPlace = originAutocomplete.getPlace();
+
+	//	if (!originPlace.geometry || !originPlace.geometry.location) {
+	//		// User entered the name of a Place that was not suggested and
+	//		// pressed the Enter key, or the Place Details request failed.
+	//		window.alert("No details available for input: '" + place.name + "'");
+	//		return;
+	//	}
+
+	//	// If the place has a geometry, then present it on a map.
+	//	if (place.geometry.viewport) {
+	//		map.fitBounds(place.geometry.viewport);
+	//	} else {
+	//		map.setCenter(place.geometry.location);
+	//		map.setZoom(17);
+	//	}
+
+	//	marker.setPosition(place.geometry.location);
+	//	marker.setVisible(true);
+
+	//	infowindowContent.children["place-name"].textContent = place.name;
+	//	infowindowContent.children["place-address"].textContent =
+	//		place.formatted_address;
+	//	infowindow.open(map, marker);
+	//});
+
+	//destinationAutocomplete.addListener("place_changed", () => {
+	//	infowindow.close();
+	//	marker.setVisible(false);
+
+	//	const place = destinationAutocomplete.getPlace();
+
+	//	if (!place.geometry || !place.geometry.location) {
+	//		// User entered the name of a Place that was not suggested and
+	//		// pressed the Enter key, or the Place Details request failed.
+	//		window.alert("No details available for input: '" + place.name + "'");
+	//		return;
+	//	}
+
+	//	// If the place has a geometry, then present it on a map.
+	//	if (place.geometry.viewport) {
+	//		map.fitBounds(place.geometry.viewport);
+	//	} else {
+	//		map.setCenter(place.geometry.location);
+	//		map.setZoom(17);
+	//	}
+
+	//	marker.setPosition(place.geometry.location);
+	//	marker.setVisible(true);
+
+	//	infowindowContent.children["place-name"].textContent = place.name;
+	//	infowindowContent.children["place-address"].textContent =
+	//		place.formatted_address;
+	//	infowindow.open(map, marker);
+	//});
+
+	//form.addEventListener("change", onChangeHandler);
 }
 
 function calculateAndDisplayRoute(
--- utils/utils.go
+++ utils/utils.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"context"
-	"log"
 
 	"googlemaps.github.io/maps"
 )
@@ -10,13 +9,11 @@ import (
 func GoogleDistanceAPI(distanceRequest *maps.DistanceMatrixRequest) (*maps.DistanceMatrixResponse, error) {
 	c, err := maps.NewClient(maps.WithAPIKey(GlobalConfig.APIKey))
 	if err != nil {
-		log.Fatalf("fatal error: %s", err)
 		return nil, err
 	}
 
 	distanceResponse, err := c.DistanceMatrix(context.Background(), distanceRequest)
 	if err != nil {
-		log.Fatalf("fatal error: %s", err)
 		return nil, err
 	}
 
